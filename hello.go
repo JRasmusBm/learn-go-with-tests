6ab7b4185a86883a9d58289b6df29eb19968b077
jrasmusbm <JRasmusBm@gmail.com>
2021-09-06 22:22:33 +0200

    Add more languages

--- hello.go
+++ hello.go
@@ -5,15 +5,32 @@ import (
 )
 
 const englishPrefix = "Hello, "
+const spanishPrefix = "¡Hola, "
+const frenchPrefix = "Bonjour, "
+const swedishPrefix = "Hej, "
 
-func Hello(name string) string {
-  if name == "" {
-    name = "world"
-  }
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
 
-	return englishPrefix + name + "!"
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishPrefix
+	case "French":
+		prefix = frenchPrefix
+  case "Swedish":
+    prefix = swedishPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -5,24 +5,45 @@ import (
 )
 
 func TestHello(t *testing.T) {
-  assertEqual := func(t testing.TB, result, expected string) {
-    t.Helper()
+	assertEqual := func(t testing.TB, result, expected string) {
+		t.Helper()
 
-    if result != expected {
-      t.Errorf("result %s got %s", result, expected)
-    }
-  }
+		if result != expected {
+			t.Errorf("result %s expected %s", result, expected)
+		}
+	}
 
-	t.Run("- greets people", func(t *testing.T) {
-		result := Hello("world")
-		expected := "Hello, world!"
+	t.Run("- greets person with specified name", func(t *testing.T) {
+		result := Hello("Peter", "")
+		expected := "Hello, Peter!"
 
 		assertEqual(t, result, expected)
 	})
 
 	t.Run("- greets world on empty string", func(t *testing.T) {
-		result := Hello("")
+		result := Hello("", "")
 		expected := "Hello, world!"
+
+		assertEqual(t, result, expected)
+	})
+
+	t.Run("- speaks Spanish when so specified", func(t *testing.T) {
+		result := Hello("Elodie", "Spanish")
+		expected := "¡Hola, Elodie!"
+
+		assertEqual(t, result, expected)
+	})
+
+	t.Run("- speaks French when so specified", func(t *testing.T) {
+		result := Hello("Marc", "French")
+		expected := "Bonjour, Marc!"
+
+		assertEqual(t, result, expected)
+	})
+
+	t.Run("- speaks Swedish when so specified", func(t *testing.T) {
+		result := Hello("Rasmus", "Swedish")
+		expected := "Hej, Rasmus!"
 
 		assertEqual(t, result, expected)
 	})
