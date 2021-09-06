2271b8a2e0dcd63862927dab22f2cd9115cc8241
jrasmusbm <JRasmusBm@gmail.com>
2021-09-06 21:49:19 +0200

    Add support for greeting name

--- hello.go
+++ hello.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func Hello() string {
-	return "Hello, world!"
+func Hello(name string) string {
+	return "Hello, " + name + "!"
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,11 +4,20 @@ import (
   "testing"
 )
 
-func TestHello(t *testing.T) {
-  result := Hello()
+func TestCanGreetWorld(t *testing.T) {
+  result := Hello("world")
   expected := "Hello, world!"
 
   if result != expected {
     t.Errorf("result %s expected %s", result, expected)
+  }
+}
+
+func TestCanGreetName(t *testing.T) {
+  result := Hello("Rasmus")
+  expected := "Hello, Rasmus!"
+
+  if result != expected {
+    t.Errorf("result %s got %s", result, expected)
   }
 }
