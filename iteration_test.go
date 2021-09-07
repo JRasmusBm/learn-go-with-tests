a0f18a6bc0a2529a5521b21df8470103436f9e9b
jrasmusbm <JRasmusBm@gmail.com>
2021-09-07 12:59:21 +0200

    Add a Benchmark

--- iteration_test.go
+++ iteration_test.go
@@ -4,6 +4,12 @@ import (
   "testing"
 )
 
+func BenchmarkRepeat(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    Repeat("a")
+  }
+}
+
 func TestRepeat(t *testing.T) {
   t.Run("- Repeats the provided symbol", func(t *testing.T) {
     result := Repeat("a")
