f79e9eab512e25bb2947eaaf1f06c0d4a4396052
jrasmusbm <JRasmusBm@gmail.com>
2021-09-07 21:43:00 +0200

    Add circle (with area)

--- geometry.go
+++ geometry.go
@@ -1,14 +1,24 @@
 package geometry
 
+import "math"
+
 type Rectangle struct {
-  Width float64
-  Height float64
+	Width  float64
+	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Circle struct {
+	Radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return (rectangle.Width + rectangle.Height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return (r.Width + r.Height)
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- geometry_test.go
+++ geometry_test.go
@@ -7,23 +7,33 @@ import (
 func TestPerimeter(t *testing.T) {
   t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
     rectangle := Rectangle{10.0, 10.0}
-    got := Perimeter(rectangle)
+    got := rectangle.Perimeter()
     want := 40.0
 
     if got != want {
-      t.Errorf("got %v got %v, given %v", got, want, rectangle)
+      t.Errorf("got %v want %v, given %v", got, want, rectangle)
     }
   })
 }
 
 func TestArea(t *testing.T) {
-  t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
+  t.Run("- Calculates the area of a rectangle", func(t *testing.T) {
     rectangle := Rectangle{10.0, 10.0}
-    got := Area(rectangle)
+    got := rectangle.Area()
     want := 20.0
 
     if got != want {
-      t.Errorf("got %v got %v, given %v", got, want, rectangle)
+      t.Errorf("got %v want %v, given %v", got, want, rectangle)
+    }
+  })
+
+  t.Run("- Calculates the area of a circle", func(t *testing.T) {
+    circle := Circle{10.0}
+    got := circle.Area()
+    want := 314.1592653589793
+
+    if got != want {
+      t.Errorf("got %v got %v, given %v", got, want, circle)
     }
   })
 }
