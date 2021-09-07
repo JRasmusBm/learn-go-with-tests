aad0b9813aff4852a97f8f9e3ef932a7c43aca17
jrasmusbm <JRasmusBm@gmail.com>
2021-09-07 21:43:27 +0200

    Refactor into shapes

--- geometry.go
+++ geometry.go
@@ -2,6 +2,10 @@ package geometry
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
--- geometry_test.go
+++ geometry_test.go
@@ -1,39 +1,39 @@
 package geometry
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestPerimeter(t *testing.T) {
-  t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
-    rectangle := Rectangle{10.0, 10.0}
-    got := rectangle.Perimeter()
-    want := 40.0
+	t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Perimeter()
+		want := 40.0
 
-    if got != want {
-      t.Errorf("got %v want %v, given %v", got, want, rectangle)
-    }
-  })
+		if got != want {
+			t.Errorf("got %v want %v, given %v", got, want, rectangle)
+		}
+	})
 }
 
 func TestArea(t *testing.T) {
-  t.Run("- Calculates the area of a rectangle", func(t *testing.T) {
-    rectangle := Rectangle{10.0, 10.0}
-    got := rectangle.Area()
-    want := 20.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
 
-    if got != want {
-      t.Errorf("got %v want %v, given %v", got, want, rectangle)
-    }
-  })
+		got := shape.Area()
 
-  t.Run("- Calculates the area of a circle", func(t *testing.T) {
-    circle := Circle{10.0}
-    got := circle.Area()
-    want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %v want %v, given %x", got, want, shape)
+		}
+	}
+	t.Run("- Calculates the area of a rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 20.0)
 
-    if got != want {
-      t.Errorf("got %v got %v, given %v", got, want, circle)
-    }
-  })
+	})
+
+	t.Run("- Calculates the area of a circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
