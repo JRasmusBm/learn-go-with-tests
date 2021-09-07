d0c2a9cac745b6a8cca49c99b4279a7a346bc77a
jrasmusbm <JRasmusBm@gmail.com>
2021-09-07 16:44:09 +0200

    Support count parameter

--- iteration.go
+++ iteration.go
@@ -1,11 +1,9 @@
 package iteration
 
-const repeatedCount = 5
+func Repeat(character string, repeatedCount int) (repeated string) {
+	for i := 0; i < repeatedCount; i++ {
+		repeated += character
+	}
 
-func Repeat(character string) (repeated string) {
-  for i := 0; i < repeatedCount; i++ {
-    repeated += character
-  }
-
-  return
+	return
 }
--- iteration_test.go
+++ iteration_test.go
@@ -1,22 +1,23 @@
 package iteration
 
 import (
-  "testing"
+	"fmt"
+	"testing"
 )
 
 func BenchmarkRepeat(b *testing.B) {
-  for i := 0; i < b.N; i++ {
-    Repeat("a")
-  }
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
 }
 
 func TestRepeat(t *testing.T) {
-  t.Run("- Repeats the provided symbol", func(t *testing.T) {
-    result := Repeat("a")
-    expected := "aaaaa"
+	t.Run("- Repeats the provided symbol n times", func(t *testing.T) {
+		result := Repeat("a", 5)
+		expected := "aaaaa"
 
-    if result != expected {
-      t.Errorf("result %s got %s", result, expected)
-    }
-  })
+		if result != expected {
+			t.Errorf("result %s got %s", result, expected)
+		}
+	})
 }
