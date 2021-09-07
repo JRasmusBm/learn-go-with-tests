7f37263552799f51c5b2e4e736359a72ea5b1847
jrasmusbm <JRasmusBm@gmail.com>
2021-09-07 21:42:17 +0200

    Refactor into Rectangle struct

--- geometry.go
+++ geometry.go
@@ -1,9 +1,14 @@
 package geometry
 
-func Perimeter(width, height float64) (perimeter float64) {
-	return 2 * (width + height)
+type Rectangle struct {
+  Width float64
+  Height float64
 }
 
-func Area(width, height float64) (perimeter float64) {
-	return (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Width + rectangle.Height)
 }
--- geometry_test.go
+++ geometry_test.go
@@ -5,23 +5,25 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-  t.Run("- Calculates the perimeter of a triangle", func(t *testing.T) {
-    got := Perimeter(10.0, 10.0)
+  t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
+    rectangle := Rectangle{10.0, 10.0}
+    got := Perimeter(rectangle)
     want := 40.0
 
     if got != want {
-      t.Errorf("got %v got %v", got, want)
+      t.Errorf("got %v got %v, given %v", got, want, rectangle)
     }
   })
 }
 
 func TestArea(t *testing.T) {
-  t.Run("- Calculates the perimeter of a triangle", func(t *testing.T) {
-    got := Area(10.0, 10.0)
+  t.Run("- Calculates the perimeter of a rectangle", func(t *testing.T) {
+    rectangle := Rectangle{10.0, 10.0}
+    got := Area(rectangle)
     want := 20.0
 
     if got != want {
-      t.Errorf("got %v got %v", got, want)
+      t.Errorf("got %v got %v, given %v", got, want, rectangle)
     }
   })
 }
