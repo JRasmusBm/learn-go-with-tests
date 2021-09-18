f3ee9a4b6c43d662d87e9743502ba5ad8b54eea8
jrasmusbm <JRasmusBm@gmail.com>
2021-09-18 20:36:36 +0200

    Add triangles

--- geometry.go
+++ geometry.go
@@ -15,6 +15,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
@@ -25,4 +30,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
--- geometry_test.go
+++ geometry_test.go
@@ -17,18 +17,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	areaTests := []struct {
-		shape Shape
-		want  float64
-	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-	}
-
-	for _, tableTest := range areaTests {
-		got := tableTest.shape.Area()
-		if got != tableTest.want {
-			t.Errorf("got %v want %v", got, tableTest.want)
+	t.Run("Table Test", func(t *testing.T) {
+		areaTests := []struct {
+			shape Shape
+			want  float64
+		}{
+			{shape: Rectangle{12, 6}, want: 72.0},
+			{shape: Circle{10}, want: 314.1592653589793},
+			{shape: Triangle{12, 6}, want: 36.0},
 		}
-	}
+
+		for _, tableTest := range areaTests {
+			got := tableTest.shape.Area()
+			if got != tableTest.want {
+				t.Errorf("%#v, got %v want %v", tableTest.shape, got, tableTest.want)
+			}
+		}
+	})
 }
