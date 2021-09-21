5c5abd102032fc7f45dc84836d4273ef97ae0d4f
jrasmusbm <JRasmusBm@gmail.com>
2021-09-21 12:37:23 +0200

    Support adding words

--- dictionary.go
+++ dictionary.go
@@ -16,3 +16,7 @@ func (d Dictionary) Lookup(word string) (string, error) {
 
 	return definition, nil
 }
+
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
--- dictionary_test.go
+++ dictionary_test.go
@@ -8,17 +8,7 @@ func TestLookup(t *testing.T) {
 	t.Run("When the key is in the dictionary", func(t *testing.T) {
 		dictionary := Dictionary{"test": "this is a test"}
 
-		got, err := dictionary.Lookup("test")
-
-		t.Run("Returns the corrresponding value", func(t *testing.T) {
-			assertEqualStrings(t, got, "this is a test")
-		})
-
-		t.Run("Does not return an error", func(t *testing.T) {
-			if err != nil {
-				t.Errorf("Got unexpected error: %#v", err)
-			}
-		})
+		assertDefinition(t, dictionary, "test", "this is a test")
 	})
 
 	t.Run("When the key is not in the dictionary", func(t *testing.T) {
@@ -32,12 +22,39 @@ func TestLookup(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	t.Run("Adds the provided word and definition to the dictionary", func(t *testing.T) {
+		dictionary := Dictionary{}
+		dictionary.Add("book", "a bound collection of pages meant for reading")
+
+		got, _ := dictionary.Lookup("book")
+		want := "a bound collection of pages meant for reading"
+
+		assertEqualStrings(t, got, want)
+	})
+}
+
 func assertEqualStrings(t testing.TB, got, want string) {
 	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Lookup(word)
+
+	if err != nil {
+		t.Errorf("Got unexpected error: %#v", err)
+	}
+
+	if got != definition {
+		t.Errorf("got %q want %q", got, definition)
+	}
+
 }
 
 func assertError(t testing.TB, got, want error) {
