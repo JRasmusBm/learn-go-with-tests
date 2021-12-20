2f5500626328f0d349d52c76b4f0facec4905369
jrasmusbm <JRasmusBm@gmail.com>
2021-12-20 22:19:55 +0100

    Add ConfigurableSleeper

--- countdown_test.go
+++ countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type SpyCountdownOperations struct {
@@ -17,6 +18,14 @@ func (s *SpyCountdownOperations) Sleep() {
 func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
 	s.Calls = append(s.Calls, write)
 	return
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 const sleep = "sleep"
@@ -58,6 +67,20 @@ Go!`
 
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	t.Run("The sleep time can be configured", func(t *testing.T) {
+		sleepTime := 5 * time.Second
+
+		spyTime := &SpyTime{}
+		sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+		sleeper.Sleep()
+
+		if spyTime.durationSlept != sleepTime {
+			t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 		}
 	})
 }
--- main.go
+++ main.go
@@ -21,6 +21,16 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := start; i > 0; i-- {
 		sleeper.Sleep()
