6d7af1c8b688fa2a83f954fb185dc39bfffd0ae0
jrasmusbm <JRasmusBm@gmail.com>
2021-12-20 22:04:16 +0100

    Sleep before every print

--- countdown_test.go
+++ countdown_test.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-func TestCountdown(t *testing.T) {
-	t.Run("Returns the highest number first", func(t *testing.T) {
-		buffer := &bytes.Buffer{}
+type SpyCountdownOperations struct {
+	Calls []string
+}
 
-		Countdown(buffer)
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const sleep = "sleep"
+const write = "write"
+
+func TestCountdown(t *testing.T) {
+
+	t.Run("Returns the string representation of a countdown", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
 		got := buffer.String()
 		want := `3
@@ -18,6 +35,28 @@ func TestCountdown(t *testing.T) {
 Go!`
 
 		if got != want {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+	t.Run("Calls sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		got := spySleepPrinter.Calls
+		want := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
 	})
--- main.go
+++ main.go
@@ -4,18 +4,33 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const start = 3
 const finalWord = "Go!"
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct {
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := start; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+
+	sleeper.Sleep()
 	fmt.Fprintf(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	Countdown(os.Stdout, &DefaultSleeper{})
 }
