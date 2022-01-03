57f6b1dcf2cd19d14141c49e3af647b82ff93c5a
jrasmusbm <JRasmusBm@gmail.com>
2022-01-03 22:00:57 +0100

    Extract getValue function
    **Why** is the change needed?
    
    So that the reflection step is clearer in the main code.

--- walk.go
+++ walk.go
@@ -3,11 +3,7 @@ package walk
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -19,4 +15,15 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	if val.Kind() == reflect.Ptr {
+		return val.Elem()
+	}
+
+	return val
+
 }
