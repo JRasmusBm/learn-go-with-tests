685c78a53122009494dd7dcca5a101cb8c802175
jrasmusbm <JRasmusBm@gmail.com>
2022-01-03 21:38:19 +0100

    Support multi-field slices

--- walk_test.go
+++ walk_test.go
@@ -6,20 +6,39 @@ import (
 )
 
 func TestWalk(t *testing.T) {
-	t.Run("- Walks a string", func(t *testing.T) {
-		var got []string
-		want := []string{"Rasmus"}
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "Struct with one string field",
+			Input: struct {
+				Name string
+			}{"Rasmus"},
+			ExpectedCalls: []string{"Rasmus"},
+		},
+		{
+			Name: "Struct with two string fields",
+			Input: struct {
+				Name    string
+				Surname string
+			}{"Rasmus", "Bergström"},
+			ExpectedCalls: []string{"Rasmus", "Bergström"},
+		},
+	}
 
-		x := struct {
-			Name string
-		}{want[0]}
+	for _, testCase := range cases {
+		t.Run(testCase.Name, func(t *testing.T) {
+			var got []string
 
-		walk(x, func(input string) {
-			got = append(got, input)
+			walk(testCase.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, testCase.ExpectedCalls) {
+				t.Errorf("Wrong calls, got %v want %v", got, testCase.ExpectedCalls)
+			}
 		})
-
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Wrong calls, got %v want %v", got, want)
-		}
-	})
+	}
 }
