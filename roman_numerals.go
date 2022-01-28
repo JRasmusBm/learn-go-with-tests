3dcf2fb0d83653672448a27347696698a165aade
jrasmusbm <JRasmusBm@gmail.com>
2022-01-28 21:42:47 +0100

    refactor(roman): Extract roman numerals type

--- roman_numerals.go
+++ roman_numerals.go
@@ -2,26 +2,26 @@ package roman_numerals
 
 import "strings"
 
+type RomanNumeral struct {
+	Value  int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	for arabic > 0 {
-		switch {
-		case arabic > 9:
-			result.WriteString("X")
-			arabic -= 10
-		case arabic > 8:
-			result.WriteString("IX")
-			arabic -= 9
-		case arabic > 4:
-			result.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			result.WriteString("IV")
-			arabic -= 4
-		default:
-			result.WriteString("I")
-			arabic -= 1
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
