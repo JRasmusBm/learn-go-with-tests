7fd3bca00a683ee6633d5259e4a6d63f64061a11
jrasmusbm <JRasmusBm@gmail.com>
2022-01-28 21:48:42 +0100

    feat(roman): Support all known roman numerals

--- roman_numerals.go
+++ roman_numerals.go
@@ -8,6 +8,12 @@ type RomanNumeral struct {
 }
 
 var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
 	{50, "L"},
 	{40, "XL"},
 	{10, "X"},
--- roman_numerals_test.go
+++ roman_numerals_test.go
@@ -26,6 +26,17 @@ func TestRomanNumerals(t *testing.T) {
 			{arabic: 47, want: "XLVII"},
 			{arabic: 49, want: "XLIX"},
 			{arabic: 50, want: "L"},
+			{arabic: 90, want: "XC"},
+			{arabic: 100, want: "C"},
+			{arabic: 400, want: "CD"},
+			{arabic: 500, want: "D"},
+			{arabic: 798, want: "DCCXCVIII"},
+			{arabic: 900, want: "CM"},
+			{arabic: 1000, want: "M"},
+			{arabic: 1006, want: "MVI"},
+			{arabic: 1984, want: "MCMLXXXIV"},
+			{arabic: 3999, want: "MMMCMXCIX"},
+			{arabic: 2014, want: "MMXIV"},
 		}
 
 		for _, testEntry := range testTable {
