70963d54d1cdb058a18f687af5aa78cd2d46f9ba
jrasmusbm <JRasmusBm@gmail.com>
2022-01-28 21:32:33 +0100

    refactor(roman): Bring 4 check into loop
    **Why** is the change needed?
    
    So that it works with the existing pattern.

--- roman_numerals.go
+++ roman_numerals.go
@@ -5,11 +5,11 @@ import "strings"
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
-	if arabic == 4 {
-		return "IV"
-	}
-
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
 		result.WriteString("I")
 	}
 
